a6bdec8cc2dc5b81250b26b7947564e462796b49
varchasvipandey <varchasvip@gmail.com>
2023-01-21 11:52:45 +0530

    update: error fix in catch

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ try {
 
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
-} catch (e) {
-  core.setFailed(e.message);
+} catch (error) {
+  core.setFailed(error.message);
 }
